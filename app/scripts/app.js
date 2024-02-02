ae925d185327aade22da7a7bd8df5b8150a815f6
Deepika Sriram <deepika.sriram1@freshworks.com>
2024-02-02 21:42:28 +0530

    Basic testing of the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,17 +2,43 @@ var client;
 
 init();
 
+let ticketData;
+let ticket_id;
+
 async function init() {
   client = await app.initialized();
-  client.events.on('app.activated', renderText);
+  client.events.on('app.activated', async () => {
+    try {
+      ticketData = await client.data.get('ticket');
+      ticket_id = ticketData.ticket.id;
+      console.log('Ticket id is : ' + ticket_id);
+      setupApp();
+    } catch (error) {
+      console.error(error);
+    }
+  });
 }
 
-async function renderText() {
-  const textElement = document.getElementById('apptext');
-  const contactData = await client.data.get('contact');
-  const {
-    contact: { name }
-  } = contactData;
+async function setupApp() {
+  console.log('Ticket id is : ' + ticket_id);
+  const createButton = document.getElementById('createButton');
+  // const editButton = document.getElementById('editButton');
+  // const deleteButton = document.getElementById('deleteButton');
+  const viewButton = document.getElementById('viewButton');
 
-  textElement.innerHTML = `Ticket is created by ${name}`;
+  createButton.addEventListener('click',async()=>{
+    const response = await client.request.invoke('createNote',{});
+    await client.interface.trigger('showModal',{
+      template:'./createNoteModal.html'
+    })
+    console.log(response.response);
+  })
+
+  viewButton.addEventListener('click',async()=>{
+    const response = await client.request.invoke('appendNote',{})
+    await client.interface.trigger('showModal',{
+      template:'./viewModal.html'
+    })
+    console.log(response.response);
+  })
 }
---
+++ server/server.js
@@ -0,0 +1,14 @@
+exports ={
+    createNote: async function(){
+        console.log("Hi");
+        renderData(null,'Hi');
+    },
+    appendNote: async function(){
+        console.log('vanakkam da mapla..madipakkathlendhu')
+        renderData(null,'vanakkam da mapla..madipakkathlendhu');
+    },
+    deleteNote : async function(){
+        console.log('Vanakkam da mapla..blore lendhu')
+        renderData(null,'Vanakkam da mapla..blore lendhu');
+    }
+}
